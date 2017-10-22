c34b804d7a1ccd63bd03df4c680984d1b8a8c231
NaolShow <naolyt@gmail.com>
2017-10-22 23:01:15 +0200

    Source 1.0.3
    Source 1.0.3

--- Main.cs
+++ Main.cs
@@ -18,5 +18,19 @@ namespace WolfLib
          *  
          **/
 
+        public static String VERSION = "1.0.3";
+
+        // Get version
+        public static String getVersion()
+        {
+            return VERSION;
+        }
+
+        // Get lasted version
+        public static String getLastedVersion()
+        {
+            return Web.downloadString("https://dl.dropboxusercontent.com/s/lt15uyw6ibnphg8/lastedVersion.txt?dl=0");
+        }
+
     }
 }
